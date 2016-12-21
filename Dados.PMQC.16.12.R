c3184e8e538e298e8ad90173fbdfab169c3c2697
TKarashima <ThiagoK@Thiagos-MacBook-Pro.local>
2016-12-20 23:52:38 -0200

    Change file names

--- Dados.PMQC.16.12.R
+++ Dados.PMQC.16.12.R
@@ -1,13 +1,13 @@
 library(data.table)
 library(stringi)
 
-pmqc.file <- "Dados.PMQC.16.09"
+pmqc.file <- "Dados.PMQC.16.12"
 pmqc.path <- "/Volumes/NO NAME/ExplAnalysis_PMQC/"
 mypath <- paste0(pmqc.path,pmqc.file,".csv")
 
-cclass <- rep("character",23)
-pmqc.dt <- fread(mypath,header=T,na.strings="",select=c(1:7,12:23),
-                 check.names=T,colClasses=cclass,encoding="Latin-1")
+# cclass <- rep("character",23)
+pmqc.dt <- fread(mypath,header=T,na.strings="",select=c(1,2,6,7,12:14,18,20,21),
+                 check.names=T,colClasses="character",encoding="Latin-1")
 # pmqc.dt <- fread(mypath,header=T,na.strings="",select=c(1:7,12:23),
 #                  check.names=T,colClasses=cclass,encoding="UTF-8")
 # pmqc.dt <- fread(mypath,header=T,na.strings="",select=c(1:7,12:23),
@@ -15,18 +15,18 @@ pmqc.dt <- fread(mypath,header=T,na.strings="",select=c(1:7,12:23),
 names(pmqc.dt) <- make.names(names(pmqc.dt))
 # Eliminating special characters and diacritical marks (accents)
 pmqc.dt[,Nao.conforme:=stri_trans_general(pmqc.dt[,Nao.conforme],"Latin-ASCII")]
-pmqc.dt[,Regiao.politica:=stri_trans_general(pmqc.dt[,Regiao.politica],"Latin-ASCII")]
-pmqc.dt[,Bandeira:=stri_trans_general(pmqc.dt[,Bandeira],"Latin-ASCII")]
+# pmqc.dt[,Regiao.politica:=stri_trans_general(pmqc.dt[,Regiao.politica],"Latin-ASCII")]
+# pmqc.dt[,Bandeira:=stri_trans_general(pmqc.dt[,Bandeira],"Latin-ASCII")]
 pmqc.dt[,Componente:=stri_trans_general(pmqc.dt[,Componente],"Latin-ASCII")]
 pmqc.dt[,Contratada:=stri_trans_general(pmqc.dt[,Contratada],"Latin-ASCII")]
 pmqc.dt[,Ensaio:=stri_trans_general(pmqc.dt[,Ensaio],"Latin-ASCII")]
 pmqc.dt[,Grupo.produto:=stri_trans_general(pmqc.dt[,Grupo.produto],"Latin-ASCII")]
 pmqc.dt[,Id.text:=stri_trans_general(pmqc.dt[,Id.text],"Latin-ASCII")]
-pmqc.dt[,Localidade:=stri_trans_general(pmqc.dt[,Localidade],"Latin-ASCII")]
-pmqc.dt[,Posto:=stri_trans_general(pmqc.dt[,Posto],"Latin-ASCII")]
+# pmqc.dt[,Localidade:=stri_trans_general(pmqc.dt[,Localidade],"Latin-ASCII")]
+# pmqc.dt[,Posto:=stri_trans_general(pmqc.dt[,Posto],"Latin-ASCII")]
 pmqc.dt[,Produto:=stri_trans_general(pmqc.dt[,Produto],"Latin-ASCII")]
 # pmqc.dt[,Produto:=stri_trans_general(pmqc.dt[,Produto],"Any-Hex/Unicode")]
-pmqc.dt[,Regiao:=stri_trans_general(pmqc.dt[,Regiao],"Latin-ASCII")]
+# pmqc.dt[,Regiao:=stri_trans_general(pmqc.dt[,Regiao],"Latin-ASCII")]
 
 # ensaios.dt <- data.table(Componente=pmqc.dt[,unique(Componente)],
 #                          Ensaio.=c("Aspecto","Cor","ME20C","TEth",rep("Dest",5),
@@ -48,13 +48,15 @@ x33 <- c("Aspecto","Condutividade Eletrica","Cor","Massa Especifica a 20ºC",
          "Destilacao - 85% Recuperados","Destilacao - 90% Recuperados",
          "Teor de Enxofre (D4294)","Ponto de Fulgor (D56)","Cor ASTM",
          "Teor de Enxofre (D5453)","Teor de Enxofre","Aromatico","Benzeno",
-         "IAD","MON","Olefina","RON","Saturado","Numero de Cetano Derivado")
+         "IAD","MON","Olefina","RON","Saturado","Numero de Cetano Derivado",
+         "Teor de Agua e Sedimentos","Teor de Agua","Material nao Volatil")
 
 x42 <- c("Aspecto","CElet","Cor","ME20C","MatPart","pH","TAlc","THC","TMet",
          "AguaLivre","Dest","Dest","Dest","Dest","Dest","Dest","ME20C","TEth",
          "Marcador","Dest","Dest","Dest","ME20C","Fulgor","B100","Dest","Dest",
          "Enxofre","Fulgor","CorASTM","Enxofre","Enxofre","Aromatico","Benzeno",
-         "IAD","MON","Olefina","RON","Saturado","Cetano")
+         "IAD","MON","Olefina","RON","Saturado","Cetano","TAgSed","TAg",
+         "MatNaoVol")
 
 ensaios.dt <- data.table(Componente=x33,Ensaio.=x42)
 
